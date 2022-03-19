be608318bb91491e5ea70962f91cee278d590260
Forest Belton <65484+forestbelton@users.noreply.github.com>
2022-03-19 16:21:58 -0400

    Improve look-and-feel

--- extract_from_wikipedia.js
+++ extract_from_wikipedia.js
@@ -1,3 +1,5 @@
+// NOTE: Copy/paste into developer console on Wikipedia page and then copy
+// document contents (https://en.wikipedia.org/wiki/List_of_conflicts_in_Europe)
 const rawEntries = [
   ...document.querySelectorAll(".mw-parser-output > ul > li"),
 ].map((node) => node.textContent);
